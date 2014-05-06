8fdce1427c99ee6a794179c95cdfdf7e2e3d0ccf
shota <shota@shota-PC>
2014-05-06 21:21:45 +0900

    I remove redundant words

--- IBatisQuickStartSample/src/main/java/com/ibatis/quickstart/sample/db/Connection.java
+++ IBatisQuickStartSample/src/main/java/com/ibatis/quickstart/sample/db/Connection.java
@@ -4,9 +4,9 @@ import java.sql.SQLException;
 
 public class Connection extends ConnectionCommon {
 
-	private static String URL      = "jdbc:oracle:thin:@(DESCRIPTION=(ENABLE=BROKEN)(ADDRESS_LIST=(FAILOVER=ON)(LOAD_BALANCE=ON)(ADDRESS=(PROTOCOL=TCP)(HOST=dev-crs-13-scan.dv.jp.honda.com)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=s_lcmdb)))";
-	private static String USER     = "lcmdb";
-	private static String PASSWORD = "lcmdb";
+	private static String URL      = "";
+	private static String USER     = "";
+	private static String PASSWORD = "";
 
 	protected Connection() throws SQLException {
 		super(URL, USER, PASSWORD);
